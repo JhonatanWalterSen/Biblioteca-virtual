54056f968964b5c4e216f5bb33bd23c9d042bd08
JhonatanWalterSen <jhonatanarizaq@gmail.com>
2020-04-10 03:24:58 -0500

    web terminado..

--- js/slick.js
+++ js/slick.js
@@ -1,6 +1,10 @@
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
+    slidesPerView: 3,
     direction: getDirection(),
+    320:{
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
